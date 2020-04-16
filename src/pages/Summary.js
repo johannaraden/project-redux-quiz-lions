92abae3553d31ef99e1fa183abfa2ae0e3add215
William Jensen <william.riis93@gmail.com>
2020-04-16 09:36:12 +0200

    merged development and master branches

--- src/pages/Summary.js
+++ src/pages/Summary.js
@@ -1,8 +1,8 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { quiz } from '../reducers/quiz'
-import { useHistory } from 'react-router-dom'
 import '../Styling/buttons.css'
 
 export const Summary = () => {
